0877edac2daf3f21d0cffed48a65b9cd826ff602
adm304524@nedbank.onmicrosoft.com <23650310+FrancoisSmit@users.noreply.github.com>
2023-11-14 12:33:20 +0200

    fix repo name

--- flagger/README.md
+++ flagger/README.md
@@ -55,7 +55,7 @@ helm upgrade -i flagger flagger/flagger \
 ```
 
 ```
-http://francoissmi.westus2.cloudapp.azure.com
+http://FrancoisSmit.westus2.cloudapp.azure.com
 ```
 6.  Create the Source Definition
 ```
--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find . -type f -not -path "./.git/*" -not -path "./.github/*" -exec sed -i 's/francoissmi/'$1'/g' {} + 
+find . -type f -not -path "./.git/*" -not -path "./.github/*" -exec sed -i 's/FrancoisSmit/'$1'/g' {} + 
